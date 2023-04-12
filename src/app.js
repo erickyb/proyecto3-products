b44314dea1d2c52adbf37fb0173b189a1ae8fb72
erickyb <richardyb1985@gmail.com>
2023-04-11 22:18:36 -0500

    bd

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require("express")
 const cors = require('cors')
 
 
-
 const { port } = require('../config')
 
 const app = express()
 
 //mi codigo
+db = require('./utils/database')
+
+
 app.get('/', (req, res) => {
     res.json({message:'server ok'})
 })
 
 
-
+app.use('/api/v1/products', productRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
